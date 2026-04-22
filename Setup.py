77a938d3df5c928b755a02f9649437c16a172d79
crgv7 <dangeveryrgooddog@gmail.com>
2026-04-22 16:40:34 -0400

    refactoring

--- Setup.py
+++ Setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.8' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
+VERSION = '1.2.0' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
 PACKAGE_NAME = 'Twotuples' #Debe coincidir con el nombre de la carpeta 
 AUTHOR = 'Carlos Garcia Vargas y Orlando Toledano' #Modificar con vuestros datos
 AUTHOR_EMAIL = '' #Modificar con vuestros datos
@@ -11,7 +11,7 @@ URL = '' #Modificar con vuestros datos
 
 LICENSE = 'MIT' #Tipo de licencia
 DESCRIPTION = 'Libreria para clasificar texto' #Descripción corta
-LONG_DESCRIPTION = (HERE / "Readme.md").read_text(encoding='utf-8') #Referencia al documento README con una descripción más elaborada
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8') #Referencia al documento README con una descripción más elaborada
 LONG_DESC_TYPE = "text/markdown"
 '''
 transformers accelerate
